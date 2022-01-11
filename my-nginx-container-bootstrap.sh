3f19baebae52ab8986a092a51a91ce190117d245
brunojose1977 <brunojose1977@yahoo.com.br>
2022-01-11 14:19:12 -0300

    Atualização com as últimas correções ajustadas no ambiente dev.

--- my-nginx-container-bootstrap.sh
+++ my-nginx-container-bootstrap.sh
@@ -21,33 +21,31 @@ apt-get update -y
 #nmon
 #echo " "
 
+#Instalação do GIT
+# Instalação será feita em /usr/bin/git
 echo "Instalando o GIT"
 apt-get install -y git
-git init
-#Clonando o repositório com o usuário e o token do github
-git clone https://brunojose1977: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@github.com/brunojose1977/teste-conteiner-nginx-com-git
-cd conteiner-nginx
+
+#Configuração do usuário, o mesmo do Gitlab
 git config --global user.name "brunojose1977"
 git config --global user.email "brunojose1977@yahoo.com"
-git config --global remote.origin.url=https://github.com/brunojose1977/teste-conteiner-nginx-com-git
-git config --global branch.master.remote=origin
-git config --global branch.master.merge=refs/heads/master
 
+#Clonando o repositório com o usuário e o token do github
+git clone https://brunojose1977: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@github.com/brunojose1977/teste-conteiner-nginx-com-git
+
+#Criando um arquivo de teste para testar a integração com o repositório remoto
+cd teste-conteiner-nginx-com-git
 echo "Test from NGIX container!" > test-from-container.txt
-
 git add test-from-container.txt
-#Daqui pra baixo só funciona se adicionar a chave pública ao GITHUB da conta brunojose1977
 git commit -m "First commit from my-nginx container!"
-git remote add origin https://github.com/brunojose1977/teste-conteiner-nginx-com-git
-git push
 
-# Instalação será feita em /usr/bin/git
+#Atualizando o repositório remoto
+git push
+git status
+
 echo " "
 #echo "Ferramentas instaladas! Experimente: htop, vtop, atop, nmon!"
 echo "FIM - Verificar se a instalação do GIT ocorreu com sucesso e se o arquivo test-from-container.txt foi commitado ao repositório no GITHUB."
 echo " "
-cd /conteiner-nginx
-pwd
+
 git status
-#echo "Rodando as ferramentas!"
-#vtop
