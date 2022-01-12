7cf2608c41ab6d002f91e225c649e602a88824ff
brunojose1977 <brunojose1977@yahoo.com.br>
2022-01-12 17:21:25 -0300

    Update my-nginx-container-bootstrap.sh

--- my-nginx-container-bootstrap.sh
+++ my-nginx-container-bootstrap.sh
@@ -30,19 +30,24 @@ apt-get install -y git
 git config --global user.name "brunojose1977"
 git config --global user.email "brunojose1977@yahoo.com"
 
+#### A partir daqui todos os comando precisam ser feito na mão.
+#### Depois de muitos testes eu verifiquei que esse comando de clone rodando na barriga do conteiner invalida o token para o uso de todos os repositórios.
+#### Depois de invalidado é preciso gerar um novo TOKEN e regerar todos os clones novamente.
+#### Como melhoria seria bom colocar esse token em variável na chamada do DOCKER RUN com parâmetro -e token=XXXXXXXXXX
+
 #Clonando o repositório que guarda os artefatos que são gerados a partir do conteiner nginx
-git clone https://brunojose1977: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@github.com/brunojose1977/nginx-conteiner-integration-test.git
+#### git clone https://brunojose1977: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@github.com/brunojose1977/nginx-conteiner-integration-test.git
 
 #Criando um arquivo de teste para testar a integração com o repositório remoto
-cd nginx-conteiner-integration-test
-echo "Test from NGIX container!" > test-from-container.txt
-git add test-from-container.txt
-git commit -m "First commit from my-nginx container!"
+#### cd nginx-conteiner-integration-test
+#### echo "Test from NGIX container!" > test-from-container.txt
+#### git add test-from-container.txt
+#### git commit -m "First commit from my-nginx container!"
 
 #Atualizando o repositório remoto
-git push
-git status
+#### git push
+#### git status
 
-echo "Verificar no repositório brunojose1977/nginx-conteiner-integration-test o arquivo test-from-container.txt foi criado!"
+#### echo "Verificar no repositório brunojose1977/nginx-conteiner-integration-test o arquivo test-from-container.txt foi criado!"
 echo " ** FIM ** "
 echo " "
